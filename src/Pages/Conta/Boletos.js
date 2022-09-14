ad36ef0027974b3eb68bfa6d14235ad81424733e
renanfort <renan_fortkamp@estudante.sesisenai.org.br>
2022-09-13 23:25:59 -0300

    criado estrutura de todas as telas restantes

---
+++ src/Pages/Conta/Boletos.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Boletos() {
+  return (
+    <View>
+      <Text>Boletos</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
